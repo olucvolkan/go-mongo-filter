adb829f484b863be614fe9004b4b46d10c7de37a
Volkan Oluc <volkan@24saatteis.com>
2021-12-12 16:19:46 +0300

    mongo test added

---
+++ mongo_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type mockRepo struct{}
+
+func (mr *mockRepo) FindRecordsWithCreatedAtAndTotalCounts(params *FindRecordsWithCreatedAtAndTotalCountsParams) ([]Row, error) {
+	var result []Row
+	return result, nil
+}
+
+func TestMongoHandlerRequest_UnmarshalJSON(t *testing.T) {
+	var jsonStr = []byte(`{
+    "startDate": "2016-01-26",
+    "endDate": "2018-02-02",
+    "minCount": 2700,
+    "maxCount": 3000
+}`)
+	req, err := http.NewRequest("POST", "/mongo", bytes.NewBuffer(jsonStr))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(buildMongoHandler(&mockRepo{}))
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	expected := 0
+	var mongoHandlerResponse MongoHandlerResponse
+	json.Unmarshal([]byte(rr.Body.String()), &mongoHandlerResponse)
+	if mongoHandlerResponse.Code != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
